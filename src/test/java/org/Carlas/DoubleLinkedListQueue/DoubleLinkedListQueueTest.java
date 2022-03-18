4095a5e5e1e422dfe3c118ffe9c275b1a08edf66
carlalo4 <carlalorido@uma.es>
2022-03-18 17:52:11 +0100

    beforeEach y AfterEach añadidos

--- src/test/java/org/Carlas/DoubleLinkedListQueue/DoubleLinkedListQueueTest.java
+++ src/test/java/org/Carlas/DoubleLinkedListQueue/DoubleLinkedListQueueTest.java
@@ -1,7 +1,25 @@
 package org.Carlas.DoubleLinkedListQueue;
 
+import org.Carlas.DequeNode.DequeNode;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DoubleLinkedListQueueTest {
+    private DoubleLinkedListQueue<Integer> linkedListQueue;
+
+    @BeforeEach
+    private void setup() {
+        linkedListQueue = new DoubleLinkedListQueue<Integer>();
+    }
+
+    @AfterEach
+    private void finish() {
+        linkedListQueue = null;
+    }
+
+
 
 }
